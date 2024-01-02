b4f03e2abe4bd3daaf97c96118333f2e0250ec42
Nicolay Rusnachenko <kolyarus@yandex.ru>
2024-01-02 23:36:37 +0000

    Refactoring prompt iterator (detach from the CSV provider). Link `input_formatters`.

--- annotate.py
+++ annotate.py
@@ -5,35 +5,13 @@ from arekit.common.pipeline.base import BasePipelineLauncher
 from arekit.common.pipeline.context import PipelineContext
 from arekit.contrib.utils.pipelines.items.text.terms_splitter import TermsSplitterParser
 
+from src.data_service import DataService
 from src.entity import IndexedEntity
 from src.json_service import JsonlService
 from src.pandas_service import PandasService
 from src.pipeline.dp import DeepPavlovNERPipelineItem
 from src.pipeline.entity_list import HandleListPipelineItem
 from src.utils import IdAssigner
-
-parser = argparse.ArgumentParser(description="Apply NER annotation")
-
-parser.add_argument('--model', dest='model', type=str, default="ner_ontonotes_bert_mult")
-parser.add_argument('--del-meta', dest="del_meta", type=list, default=["parent_ctx"])
-parser.add_argument('--csv-sep', dest='csv_sep', type=str, default='\t')
-parser.add_argument('--prompt', dest='prompt', type=str, default="{text}")
-parser.add_argument('--src', dest='src', type=str, default="./data/test.csv")
-parser.add_argument('--output', dest='output', type=str, default=None)
-
-args = parser.parse_args()
-
-# Provide the default output.
-if args.output is None:
-    args.output = ".".join(args.src.split('.')[:-1]) + "-converted.jsonl"
-
-input_formatters = {
-    "csv": lambda: PandasService.iter_prompts(pd.read_csv(args.src, sep=args.csv_sep), prompt=args.prompt)
-}
-
-output_formatters = {
-    "jsonl": lambda dicts_it: JsonlService.write(output=args.output, lines_it=dicts_it)
-}
 
 
 def iter_annotated_data(texts_it):
@@ -52,6 +30,29 @@ def iter_annotated_data(texts_it):
         yield d
 
 
+parser = argparse.ArgumentParser(description="Apply NER annotation")
+
+parser.add_argument('--model', dest='model', type=str, default="ner_ontonotes_bert_mult")
+parser.add_argument('--del-meta', dest="del_meta", type=list, default=["parent_ctx"])
+parser.add_argument('--csv-sep', dest='csv_sep', type=str, default='\t')
+parser.add_argument('--prompt', dest='prompt', type=str, default="{text}")
+parser.add_argument('--src', dest='src', type=str, default="./data/test.csv")
+parser.add_argument('--output', dest='output', type=str, default=None)
+
+args = parser.parse_args()
+
+# Provide the default output.
+if args.output is None:
+    args.output = ".".join(args.src.split('.')[:-1]) + "-converted.jsonl"
+
+input_formatters = {
+    "csv": lambda: PandasService.iter_rows_as_dict(df=pd.read_csv(args.src, sep=args.csv_sep))
+}
+
+output_formatters = {
+    "jsonl": lambda dicts_it: JsonlService.write(output=args.output, lines_it=dicts_it)
+}
+
 # Application of the NER for annotation texts.
 pipeline = [
     TermsSplitterParser(),
@@ -62,6 +63,7 @@ pipeline = [
     HandleListPipelineItem(map_item_func=lambda _, t: f"[{t.Type}]" if isinstance(t, IndexedEntity) else t),
 ]
 
-prompts_it = PandasService.iter_prompts(df=pd.read_csv(args.src, sep=args.csv_sep), prompt=args.prompt)
+texts_it = input_formatters["csv"]()
+prompts_it = DataService.iter_prompt(data_dict_it=texts_it, prompt=args.prompt)
 ctxs_it = iter_annotated_data(texts_it=prompts_it)
 output_formatters["jsonl"](dicts_it=ctxs_it)
--- src/pandas_service.py
+++ src/pandas_service.py
@@ -1,20 +1,9 @@
 from tqdm import tqdm
 
-from src.utils import iter_params, iter_with_ids
-
 
 class PandasService(object):
     
     @staticmethod
-    def iter_texts(df, cols=None):
-        for row_ind, data in tqdm(df.iterrows(), total=len(df)):
-            data_dict = data.to_dict()
-            yield [data_dict[c] for c in cols]
-
-    @staticmethod
-    def iter_prompts(df, prompt):
-        column_names = list(iter_params(prompt))
-        it = PandasService.iter_texts(df=df, cols=column_names)
-        for row_id, item in iter_with_ids(it):
-            fmt_d = {col_name: item[col_ind] for col_ind, col_name in enumerate(column_names)}
-            yield row_id, prompt.format(**fmt_d)
+    def iter_rows_as_dict(df):
+        for _, data in tqdm(df.iterrows(), total=len(df)):
+            yield data.to_dict()
