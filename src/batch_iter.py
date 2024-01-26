fa13e4159c56823e2baee37bef9c4a7c1237ece5
Nicolay Rusnachenko <kolyarus@yandex.ru>
2024-01-26 23:16:02 +0000

    Provide another unit-test for batching via DeepPavlov

--- src/batch_iter.py
+++ src/batch_iter.py
@@ -1,10 +1,12 @@
 class BatchIterator:
 
-    def __init__(self, data_iter, batch_size):
+    def __init__(self, data_iter, batch_size, end_value=None):
         assert(isinstance(batch_size, int) and batch_size > 0)
+        assert(callable(end_value) or end_value is None)
         self.__data_iter = data_iter
         self.__index = 0
         self.__batch_size = batch_size
+        self.__end_value = end_value
 
     def __iter__(self):
         return self
@@ -24,4 +26,7 @@ class BatchIterator:
             self.__index += 1
             return buffer
 
-        raise StopIteration
+        if self.__end_value is None:
+            raise StopIteration
+        else:
+            return self.__end_value()
---
+++ test/test_ner_batching_internal.py
@@ -0,0 +1,45 @@
+import json
+import unittest
+from pathlib import Path
+from typing import Union
+
+from deeppavlov import build_model, Chainer
+from deeppavlov.core.data.utils import jsonify_data
+
+from data import TEXTS
+from src.batch_iter import BatchIterator
+
+
+def predict_on_stream(config: Union[str, Path, dict], get_batch_func) -> None:
+    """Make a prediction with the component described in corresponding configuration file."""
+
+    model: Chainer = build_model(config)
+
+    args_count = len(model.in_x)
+    while True:
+        batch = get_batch_func()
+
+        if not batch:
+            break
+
+        args = []
+        for i in range(args_count):
+            args.append(batch[i::args_count])
+
+        res = model(*args)
+        if len(model.out_params) == 1:
+            res = [res]
+        for res in zip(*res):
+            res = json.dumps(jsonify_data(res), ensure_ascii=False)
+            yield res
+
+
+class TestNerBatching(unittest.TestCase):
+
+    def test_2(self):
+        batch_size = 4
+        batch_it = BatchIterator(iter(TEXTS), batch_size=batch_size, end_value=lambda: None)
+        for data in predict_on_stream("ner_ontonotes_bert_mult", get_batch_func=lambda: next(batch_it)):
+            print("---")
+            print(data)
+
