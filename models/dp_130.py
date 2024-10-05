aea65c510cba0d9d727f042f178aeb57190f0c66
Nicolay Rusnachenko <kolyarus@yandex.ru>
2024-10-05 16:51:31 +0100

    #9 simplified

--- models/dp_130.py
+++ models/dp_130.py
@@ -1,15 +1,13 @@
 # This is an adapter implementation for dp_130
 # deeppavlov==1.3.0
 
-import importlib
+from deeppavlov import build_model
 from bulk_ner.src.ner.base import BaseNER
 
 
 class DeepPavlovNER(BaseNER):
 
     def __init__(self, model, download=True, install=True):
-        deeppavlov = importlib.import_module("deeppavlov")
-        build_model = deeppavlov.build_model
         self.__ner_model = build_model(model, download=download, install=install)
 
     def _forward(self, sequences):
